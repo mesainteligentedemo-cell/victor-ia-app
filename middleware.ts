913388082937dc4d139bf3bc202af9251411fbc7
Victor IA <mesainteligentedemo@gmail.com>
2026-06-11 16:25:47 -0500

    fix: install missing dependencies and update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/public/*"],
-  ignoredRoutes: ["/api/webhooks/*"]
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\.[\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ]
 };
